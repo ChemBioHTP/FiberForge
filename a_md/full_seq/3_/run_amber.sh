c1f15ad3de53124387d250e0a1c4c89e0d5c4712
Kieran Nehil-Puleo <nehilpkd@gw342.vampire>
2023-08-18 11:01:28 -0500

    added new file structure for adsorption and a_md

---
+++ a_md/full_seq/3_/run_amber.sh
@@ -0,0 +1,22 @@
+#!/bin/bash
+#SBATCH --job-name=foo_amb
+#SBATCH --account=csb_gpu_acc
+#SBATCH --partition=maxwell
+#SBATCH --gres=gpu:1
+#SBATCH --nodes=1
+#SBATCH --ntasks-per-node=1
+#SBATCH --mem=10G
+#SBATCH --time=05:00:00
+#SBATCH --no-requeue
+#SBATCH --export=NONE
+#SBATCH --output=test_%J.txt
+
+WORKING_DIR=/home/nehilpkd/projects/biomatsims/a_md/3_
+cd $WORKING_DIR
+
+
+source /home/shaoq1/bin/amber_env/amber-accre.sh
+
+pmemd.cuda -O -i heat.in -o heat.out -p ../*.prmtop -c ../3_/*.rst -r heat.rst -ref ../3_/*.rst
+
+echo "The End"
---
+++ a_md/full_seq/6_/run_amber.sh
@@ -0,0 +1,22 @@
+#!/bin/bash
+#SBATCH --job-name=foo_amb
+#SBATCH --account=csb_gpu_acc
+#SBATCH --partition=maxwell
+#SBATCH --gres=gpu:1
+#SBATCH --nodes=1
+#SBATCH --ntasks-per-node=1
+#SBATCH --mem=10G
+#SBATCH --time=05:00:00
+#SBATCH --no-requeue
+#SBATCH --export=NONE
+#SBATCH --output=test_%J.txt
+
+WORKING_DIR=/home/nehilpkd/projects/foo/6_
+cd $WORKING_DIR
+
+
+source /home/shaoq1/bin/amber_env/amber-accre.sh
+
+pmemd -O -i amd.in -o amd.out -p ../*.prmtop -c ../6_/eq.rst -r prod.nc
+
+echo "The End"
